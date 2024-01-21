245c7ba0c30047299fcb0bf72e24ccf97c057d79
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-01-20 22:09:42 -0500

    feat: add game board with squares

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,58 @@
+import { Option } from 'effect';
+import { constVoid } from 'effect/Function';
 import { useState } from 'react';
+import { match } from 'ts-pattern';
 
-export function App() {
-  const [count, setCount] = useState(0);
+interface SquareProps {
+  value: Option.Option<string>;
+  onSquareClick: () => void;
+}
+
+function Square({ value, onSquareClick }: SquareProps) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {Option.getOrNull(value)}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState<readonly Option.Option<string>[]>(
+    Array(9).fill(Option.none())
+  );
+
+  function handleClick(i: number) {
+    Option.match(squares[i], {
+      onNone: () => {
+        const nextSquares = [...squares];
+        nextSquares[i] = match(xIsNext)
+          .with(true, () => Option.some('X'))
+          .otherwise(() => Option.some('O'));
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+      },
+      onSome: constVoid,
+    });
+  }
 
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
   );
 }
